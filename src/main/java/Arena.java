9211c1e11f409986c5ed0d7f44c4dd57ed8a4705
jorpac99 <jorpac99@gmail.com>
2019-02-25 20:57:15 +0000

    Update

--- src/main/java/Arena.java
+++ src/main/java/Arena.java
@@ -7,14 +7,34 @@ import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.screen.Screen;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Arena {
     private int height, width;                  //  Generally constants(dimensions of the screen), but for usability purposes, can be changed
     Hero hero = new Hero(0, 0);
+    private List<Wall> walls;
     Screen screen;
     public Arena(int height, int width) {
         this.height = height;
         this.width = width;
+        this.walls = createWalls();
+    }
+
+    private List<Wall> createWalls() {
+        List<Wall> walls = new ArrayList<>();
+
+        for (int c = 0; c < width; c++) {
+            walls.add(new Wall(c, 0));
+            walls.add(new Wall(c, height - 1));
+        }
+
+        for (int r = 1; r < height - 1; r++) {
+            walls.add(new Wall(0, r));
+            walls.add(new Wall(width - 1, r));
+        }
+
+        return walls;
     }
 
     public int getHeight() {
